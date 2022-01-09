4e8d4a9d69c4a69bf1681383140774c0ef3ec437
eqy <eddieyan101@gmail.com>
2022-01-08 16:38:27 -0800

    Update gif.sh

--- gif.sh
+++ gif.sh
@@ -1,1 +1,3 @@
 ffmpeg -i nyokengif-889443730.mp4 -filter_complex "[0:v]fps=fps=30[f];[f]crop=389:367:523:189" -ss 0.433 -t 0.810 -f gif nyokenpoint.gif
+
+ffmpeg -i tensor\ faces-875529267.mp4 -ss 6.025 -t 0.386 -filter_complex "[0:v]crop=56:56:1067:547,fps=90,split=2[a1][a2];[a1]reverse[r];[a2][r]concat=n=2:v=1:a=0,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" smug.gif
